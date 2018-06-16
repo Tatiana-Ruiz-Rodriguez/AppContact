1d49a938ab44ca1ec8d7075720175e8d292249ee
Tatiana-Ruiz-Rodriguez <tatianaruiz@compucentroltda.com>
2018-06-16 08:41:37 -0500

    Inicio

---
+++ AppContact/AppContact.Android/Services/FileHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using AppContacts.Services;
+using Xamarin.Forms;
+using AppContact.Droid.Services;
+
+
+[assembly: Dependency(typeof(FileHelper))]
+namespace AppContact.Droid.Services
+{
+    public class FileHelper : IFileHelper
+    {
+        public string GetLocalFilePath(string fileName)
+        {
+            string path =
+                Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+            return Path.Combine(path, fileName);
+        }
+    }
+}
---
+++ AppContact/AppContact/Data/ContactsDataBase.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AppContact.Models;
+
+namespace AppContact.Data
+{
+    using SQLite;
+    using System.Threading.Tasks;
+
+    public class ContactsDataBase
+    {
+
+        private readonly SQLiteAsyncConnection dataBase;
+
+        public ContactsDataBase(string dbPath)
+        {
+            dataBase = new SQLiteAsyncConnection(dbPath);
+            dataBase.CreateTableAsync<Contact>().Wait();
+        }
+
+        public async Task<List<Contact>> GetItemsAsync()
+        {
+            var data = await dataBase.Table<Contact>().ToListAsync();
+            return data;
+        }
+
+        public Task<Contact> GetItemAsync(int Id)
+        {
+            return dataBase.Table<Contact>()
+                .Where(c=>c.ID == Id)
+                .FirstOrDefaultAsync();
+        }
+
+        public Task<int> SaveItemAsync(Contact item)
+        {
+            if (item.ID != 0)
+            {
+                return dataBase.UpdateAsync(item);
+            }
+            else
+            {
+                return dataBase.InsertAsync(item);
+            }
+        }
+
+        public Task<int> DeleteItemAsync(Contact item)
+        {
+
+            return dataBase.DeleteAsync(item);
+
+        }
+
+    }
+}
